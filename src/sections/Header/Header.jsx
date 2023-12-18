efc8c895bde92ccdb570b818b522c2991fba79a2
al.rod@outlook.fr <al.rod@outlook.fr>
2023-12-18 17:54:42 +0100

    add header and title

---
+++ src/sections/Header/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css'
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <div className='headerContainer'>
+        <div className='HeaderContent'>Home</div>
+        <div className='HeaderContent'>Projects</div>
+        <div className='HeaderContent'>Contact</div>
+      </div>
+    </div>
+  )
+}
